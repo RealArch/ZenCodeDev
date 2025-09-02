8b3716670e0eb449f6a3c9858b3e7e5f1c8db5c3
Rafael Alvarez <rafael8721693@gmail.com>
2025-09-01 20:40:25 -0400

    s

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -3,6 +3,7 @@ import { Component, Inject, PLATFORM_ID, signal } from '@angular/core';
 import { getAnalytics, logEvent } from '@angular/fire/analytics';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
+import { FirebaseApp } from '@angular/fire/app';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +13,15 @@ import { environment } from '../environments/environment';
 })
 export class App {
   protected readonly title = signal('web');
-  constructor() {
-    console.log(environment.env)
-  }
-  // constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-  //   if (isPlatformBrowser(this.platformId)) {
-  //     const analytics = getAnalytics();
-  //     logEvent(analytics, 'app_start'); // Ejemplo de evento
-  //   }
 
-  // }
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private app: FirebaseApp) { // Inyecta FirebaseApp
+    console.log(environment.env)
+    if (isPlatformBrowser(this.platformId)) {
+      const analytics = getAnalytics(this.app); // Pasa la instancia de la app
+      logEvent(analytics, 'app_start');
+    }
+  }
 }
+
+
